06f4420a2bd752455a566d9e012dec2524d90369
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 00:53:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,15 +6,17 @@ end
 
 def using_sort(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  
+  array.sort 
 end
 
 def using_reverse(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.reverse 
 end
 
 def using_first(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  
 end
 
 def using_last(array)
